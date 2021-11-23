a2e3529b049f39c8d4b9f553fea3b632346e215b
sathamverticurl <sathamhussain.maheen@verticurl.com>
2021-11-23 13:00:04 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, SidebarComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SidebarComponent,
+    SectionComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
